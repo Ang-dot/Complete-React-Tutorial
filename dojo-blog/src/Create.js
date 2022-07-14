752efb5d95128f3436d99bb206b9f7dbb4e5bda0
Ang-dot <weoyyang00@gmail.com>
2022-07-14 23:15:49 +0800

    Add a "Create" React COmponent

---
+++ dojo-blog/src/Create.js
@@ -0,0 +1,11 @@
+const Create = () => {
+    return ( 
+        <div>
+            <div className="class">
+                <h2>Add a New Blog</h2>
+            </div>
+        </div>
+    );
+}
+ 
+export default Create;
