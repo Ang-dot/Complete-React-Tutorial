1ebd668a4762f7e79cb7fa8fcda0ef8073af34b4
Ang-dot <weoyyang00@gmail.com>
2022-07-15 00:20:02 +0800

    Config useFetch function
    Imitate real life date loading duration

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -6,32 +6,33 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const abortController = new AbortController();
+        setTimeout(() => {
+            const abortController = new AbortController();
 
-        fetch(url, { signal: abortController.signal })
-        .then(res => {
-            if (!res.ok) {
-            throw Error('could not fetch the data for that resource');
-            }
-            return res.json();
-        })
-        .then(data => {
-            setData(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            if (err.name !== 'AbortError') {
+            fetch(url, { signal: abortController.signal })
+            .then(res => {
+                if (!res.ok) {
+                throw Error('could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
                 setIsPending(false);
-                setError(err.message);
-            }
-        })
+                setError(null);
+            })
+            .catch(err => {
+                if (err.name !== 'AbortError') {
+                    setIsPending(false);
+                    setError(err.message);
+                }
+            })
 
-        return () => abortController.abort();
+            return () => abortController.abort();
+        }, 350);
     }, [url]);
 
     return { data, isPending, error };
 };
-
 
 export default useFetch;
